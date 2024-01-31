2b5ab41b1530ddc7be5343e698d7e976452809aa
Aghalarova Aygun <aygunea@code.local>
2024-01-31 14:16:51 +0400

    part 5 completed

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// const swiperEl = document.querySelector('swiper-container');
+// const swiper = swiperEl.swiper;
+
+var swiper = new Swiper('.swiper-container', {
+    // Gerekli ayarlar buraya eklenebilir
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
