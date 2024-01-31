29a2148b0fbec7760a3b796cc2befe0c07fc9487
aygun aghalarova <agalarova.aygunn@gmail.com>
2024-02-01 00:12:51 +0400

    part 6 is completed

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-// const swiperEl = document.querySelector('swiper-container');
-// const swiper = swiperEl.swiper;
-
-var swiper = new Swiper('.swiper-container', {
-    // Gerekli ayarlar buraya eklenebilir
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
